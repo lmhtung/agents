0aa65d64d7d35f821ca840f783190c2a7bc80cdb
Le Mau Hoang Tung <lmhtung@gmail.com>
2026-02-24 09:32:16 +0700

    Update files and folders

--- rag/initialize.py
+++ rag/initialize.py
@@ -1,8 +1,8 @@
 import os
 from pathlib import Path
-# from langchain_huggingface import HuggingFaceEmbeddings
-# from langchain_qdrant.fastembed_sparse import FastEmbedSparse
-# from qdrant_client import QdrantClient
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_qdrant.fastembed_sparse import FastEmbedSparse
+from qdrant_client import QdrantClient
 
 DOCS_DIR = "docs"  # Directory containing your pdf files
 MARKDOWN_DIR = "markdown" # Directory containing the pdfs converted to markdown
@@ -12,3 +12,11 @@ CHILD_COLLECTION = "document_child_chunks"
 os.makedirs(DOCS_DIR, exist_ok=True)
 os.makedirs(MARKDOWN_DIR, exist_ok=True)
 os.makedirs(PARENT_STORE_PATH, exist_ok=True)
+
+from langchain_ollama import ChatOllama
+llm = ChatOllama(model="qwen3:4b-instruct-2507-q4_K_M", temperature=0)
+
+dense_embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
+sparse_embeddings = FastEmbedSparse(model_name="Qdrant/bm25")
+
+client = QdrantClient(path="qdrant_db")
